5032ba505ccd79639ac99b33c921aed5136c37c5
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2022-03-11 21:58:17 +0100

    this anahtar kelime kullanimi

---
+++ Bolum_4_OOP/D02_OOP_This_Anahtar.dart
@@ -0,0 +1,31 @@
+import 'D01_OOP.dart';
+
+void main(List<String> args) {
+  //this atahtar kelimesinin  kullanimi
+  var opel = Araba(2019, "Opel Astra", true, "Benzin & LPG");
+  print(opel);
+  opel.yasHesapla();
+}
+
+class Araba {
+  int?
+      model; // ? null olabilecegini gosterir. varsayilan deger atamamamk icin bu sekilde kullandik.
+  String? marka;
+  bool? vites;
+  String? yakit;
+
+  void yasHesapla() {
+    print(
+        "Arabanin Yasi :${2022 - model!}"); // ! kullanma amaci null olabilecek bir seyin hic bir zaman null almayacagi anlamina gelir.
+  }
+
+  Araba(int modelYili, String markasi, bool vitesi, String yakiti) {
+    print("-----Araba Bilgileri-----");
+    print("Markasi $markasi modeli $modelYili vites $vitesi yakiti $yakiti");
+    // this.class_da_belirtilen_degiskenler = constructor parametresindeki degiskenler
+    this.model = modelYili;
+    this.marka = markasi;
+    this.vites = vitesi;
+    this.yakit = yakiti;
+  }
+}
