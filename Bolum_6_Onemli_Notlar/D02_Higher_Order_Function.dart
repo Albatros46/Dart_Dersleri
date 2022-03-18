a077f6232d41b500910ea61d7e52a915e6be9ae8
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2022-03-18 21:36:15 +0100

    /**Higher order fuction : bir fonksiyonu parametre oalrak  alan veya  *  geriye fonksiyon donduren fonksiyonlardir.  * Her ikisini ayni anda da yapabilir.

---
+++ Bolum_6_Onemli_Notlar/D02_Higher_Order_Function.dart
@@ -0,0 +1,31 @@
+/**Higher order fuction : bir fonksiyonu parametre oalrak  alan veya
+ *  geriye fonksiyon donduren fonksiyonlardir.
+ * Her ikisini ayni anda da yapabilir.
+ */
+void main(List<String> args) {
+  List<int> liste = [12, 56, 54, 41];
+  //aslinda forEach bir fonksiyon
+  // asagidaki kisim ise lambda kullanimi ile fonksiyon
+  liste.forEach((element) {
+    print("Element : $element");
+  });
+  liste.forEach(
+      geriDon); // forEach kendisi bir fonksiyon, ben kendi yazdigim fonksiyonu parametre olarak gonderdim
+  print("------------------------");
+  forIc(liste, (int deger) {
+    print("Okunan :$deger");
+  });
+}
+
+void geriDon(int item) {
+  print("Item :$item");
+}
+
+void forIc(List<int> liste, Function callback) {
+  // parametre olarak fonksiyon da gonderdik
+  for (var i = 0; i < liste.length; i++) {
+    print("Deger :${liste[i]}");
+    //veya
+    callback(liste[i]);
+  }
+}
